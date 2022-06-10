dee4cfba6184b5a1f72bd3cba1c38ffd6a1b78e4
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-06-11 01:18:30 +0500

    navigation,home and products component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './module/home/home.component';
+import { NavigationComponent } from './shared/navigation/navigation.component';
+import { OrderComponent } from './module/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavigationComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
