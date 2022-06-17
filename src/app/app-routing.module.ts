d1c5424df15514b0cea5a5c96b106c951e300fc7
Khalil Asif <58604863+devkhalil1337@users.noreply.github.com>
2022-06-17 14:13:05 +0500

    Home,Products and contact us modules added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [];
+import { ContactComponent } from './modules/contact/contact.component';
+import { HomeComponent } from './modules/home/home.component';
+import { ProductsComponent } from './modules/products/products.component';
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'products',
+  component: ProductsComponent
+},
+{
+  path: 'contact-us',
+  component: ContactComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { FooterComponent } from './modules/footer/footer.component';
+import { NavigationComponent } from './modules/navigation/navigation.component';
+import { ProductsComponent } from './modules/products/products.component';
+import { ContactComponent } from './modules/contact/contact.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavigationComponent,
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
