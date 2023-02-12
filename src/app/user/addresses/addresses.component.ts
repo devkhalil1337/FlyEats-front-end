a599042acea228b12953a7875b373a2770350f1b
devkhalil1337 <devkhalil1337@gmail.com>
2023-02-12 17:42:30 +0500

    Addresses API integrated

--- src/app/user/addresses/addresses.component.ts
+++ src/app/user/addresses/addresses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { address } from 'src/app/mock-api/address';
+import { AddressesService } from './addresses.service';
 
 @Component({
   selector: 'app-addresses',
@@ -11,12 +12,25 @@ export class AddressesComponent implements OnInit {
 
   addressesList:any
 
-  constructor() {
-    this.addressesList = address
+  constructor(private addressesService:AddressesService) {
+    // this.addressesList = address
+    this.getAddresses();
    }
 
   ngOnInit(): void {
   }
 
+  getAddresses(){
+    this.addressesService.getActiveAddressesByUserId('1').subscribe(response => {
+      console.log({response})
+      this.addressesList = response;
+    })
+  }
+
+  onDeleteAddress(addressId:number){
+    this.addressesService.deleteAddress(addressId).subscribe(response => {
+      this.getAddresses();
+    })
+  }
 
 }
---
+++ src/app/user/addresses/addresses.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from 'src/app/modules/shared/api.service';
+import { map } from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressesService {
+
+  constructor(private apiService:ApiService) { }
+
+
+  getActiveAddressesByUserId(userId: string) {
+    const apiEndpoint = 'AddressBook/GetAddressesByUserId';
+    const url = `${apiEndpoint}?userId=${userId}`;
+    return this.apiService.request('get', url).pipe(
+      map((response: any) => {
+        return response.filter((elm:any) => elm.active) || [];
+      })
+    );
+  }
+  
+
+ deleteAddress(addressId:number){
+    return this.apiService.request("delete",`AddressBook/DeleteAddress/${addressId}`)
+  }
+}
