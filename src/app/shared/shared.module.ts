fb98d8866bfa92133758b1d0ee25bf731215cf92
devkhalil1337 <devkhalil1337@gmail.com>
2023-03-01 01:21:36 +0500

    moved pipes to shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FilterMenuPipe } from './pipes/filter-menu.pipe';
-import { LazyLoadingDirective } from './directive/lazy-loading.directive';
+import { LazyLoadingDirective } from './lazy-loading.directive';
 import { MycurrencyPipe } from './pipes/mycurrency.pipe';
 import { CartService } from './cart.service';
 
