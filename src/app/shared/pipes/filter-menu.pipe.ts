54d5c65a07f42c0afabebcf3a68991aff242ee8d
devkhalil1337 <devkhalil1337@gmail.com>
2023-03-04 15:19:43 +0500

    Fixed menu pipe

--- src/app/shared/pipes/filter-menu.pipe.ts
+++ src/app/shared/pipes/filter-menu.pipe.ts
@@ -9,25 +9,26 @@ export class FilterMenuPipe implements PipeTransform {
 
   transform(itemsArr: Product[], categoryId: number, orderType: string) {
     return itemsArr.filter((elm: Product) => {
+        // If the product is not in the specified category, do not show it
+        if (elm.categoryId !== categoryId) {
+          return false;
+        }
       // If the product is a delivery product, only show it if the order type is delivery
-      if (elm.isDeliveryProduct && orderType === OrderTypes.Delivery) {
-        return false;
+      if(orderType === OrderTypes.Delivery){
+        return elm.isDeliveryProduct
       }
       
       // If the product is a pickup product, only show it if the order type is pickup
-      if (elm.isPickupProduct && orderType === OrderTypes.PickUp) {
-        return false;
+      if(orderType === OrderTypes.PickUp){
+        return elm.isPickupProduct
       }
       
       // If the product is a table product, only show it if the order type is table
-      if (elm.isTableProduct && orderType === OrderTypes.Table) {
-        return false;
+      if(orderType === OrderTypes.Table){
+        return elm.isTableProduct;
       }
       
-      // If the product is not in the specified category, do not show it
-      if (elm.categoryId !== categoryId) {
-        return false;
-      }
+    
       
       // If all checks pass, show the product
       return true;
