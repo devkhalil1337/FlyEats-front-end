34d7ec39b34e6f434d502fa5f0146e045271f69f
devkhalil1337 <devkhalil1337@gmail.com>
2023-03-05 15:46:18 +0500

    Feature product module added

---
+++ src/app/shared/pipes/feature-products.pipe.ts
@@ -0,0 +1,38 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { OrderTypes } from '@enums/OrderTypeEnum';
+import { Product } from 'src/app/filters/product.model';
+
+@Pipe({
+  name: 'featureProducts'
+})
+export class FeatureProductsPipe implements PipeTransform {
+
+  transform(itemsArr: Product[], orderType: string) {
+    return itemsArr.filter((elm: Product) => {
+      // If the product is not in the specified category, do not show it
+      if (!elm.featured) {
+        return false;
+      }
+    // If the product is a delivery product, only show it if the order type is delivery
+    if(orderType === OrderTypes.Delivery){
+      return elm.isDeliveryProduct
+    }
+    
+    // If the product is a pickup product, only show it if the order type is pickup
+    if(orderType === OrderTypes.PickUp){
+      return elm.isPickupProduct
+    }
+    
+    // If the product is a table product, only show it if the order type is table
+    if(orderType === OrderTypes.Table){
+      return elm.isTableProduct;
+    }
+    
+  
+    
+    // If all checks pass, show the product
+    return true;
+  });
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,14 +4,15 @@ import { FilterMenuPipe } from './pipes/filter-menu.pipe';
 import { LazyLoadingDirective } from './lazy-loading.directive';
 import { MycurrencyPipe } from './pipes/mycurrency.pipe';
 import { CartService } from './cart.service';
+import { FeatureProductsPipe } from './pipes/feature-products.pipe';
 
 
 
 @NgModule({
-  declarations: [FilterMenuPipe, LazyLoadingDirective, MycurrencyPipe],
+  declarations: [FilterMenuPipe, LazyLoadingDirective, MycurrencyPipe, FeatureProductsPipe],
   imports: [
     CommonModule
   ],
-  exports:[FilterMenuPipe,LazyLoadingDirective,MycurrencyPipe]
+  exports:[FilterMenuPipe,LazyLoadingDirective,MycurrencyPipe,FeatureProductsPipe]
 })
 export class SharedModule { }
